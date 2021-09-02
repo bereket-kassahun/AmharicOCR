160e5d20bb8b1ee3d6516acc431bbd2ae49db72b
bereket-kassahun <bereket_kasahun@yahoo.com>
2021-09-02 15:50:54 +0300

    fix & feat: image to grayscale and opencv library loading fixed

--- app/src/main/java/com/example/amharicocr/MainActivity.java
+++ app/src/main/java/com/example/amharicocr/MainActivity.java
@@ -2,16 +2,128 @@ package com.example.amharicocr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private Button capture;
+    private Button convert;
+    private ImageView preview;
+    private Bitmap bitmap = null;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.e("OpenCV", "OpenCV loaded successfully");
+//                    imageMat=new Mat();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SimpleSample.main(new String[2]);
+//        SimpleSample.main(new String[2]);
+        capture = findViewById(R.id.capture);
+        convert = findViewById(R.id.convert);
+        preview = findViewById(R.id.preview);
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               dispatchTakePictureIntent();
+            }
+        });
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bitmap != null){
+                    Mat tmp = convertBitmapToGray(bitmap);
+                    Bitmap tmp1 = createBitmapfromMat(tmp);
+                    preview.setImageBitmap(tmp1);
+                }
+            }
+        });
     }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+//            Toast.makeText(this, "unable to open camera app", Toast.LENGTH_LONG).show();
+            Log.e("error :", e.getMessage());
+        }
+    }
+    private Mat convertBitmapToGray(Bitmap bitmap){
+        Mat ret = new Mat (bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
+        Bitmap bmp32 = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bmp32, ret);
+        Imgproc.cvtColor(ret, ret, Imgproc.COLOR_RGB2GRAY);
+        return ret;
+    }
+
+    public Bitmap createBitmapfromMat(Mat snap){
+        Bitmap bp = Bitmap.createBitmap(snap.cols(), snap.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(snap, bp);
+        return bp;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            bitmap = imageBitmap;
+            preview.setImageBitmap(imageBitmap);
+        }
+    }
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.e("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.e("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+
 }
